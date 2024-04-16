4d0a1e2b7cfb3fbb55507dfc3ecd572a99a3ad53
Raghav Dave <raghavdave@Raghavs-MacBook-Pro.local>
2024-04-16 15:25:20 -0400

    tes

--- server.js
+++ server.js
@@ -89,6 +89,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Error handler middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
