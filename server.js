ac821a36f19d14522501a657f050b2f8908caad3
Raghav Dave <raghavdave@Raghavs-MacBook-Pro.local>
2024-04-16 13:04:34 -0400

    tes

--- server.js
+++ server.js
@@ -1,47 +1,67 @@
-const express = require('express');
 const axios = require('axios');
 const tunnel = require('tunnel');
-
-const app = express();
-const port = process.env.PORT || 3000;
+const qs = require('qs');
 
 const VGS_VAULT_ID = 'tntkmaqsnf9';
 const VGS_USERNAME = 'USpDfWz23n8FGztYxzi5RNDa';
 const VGS_PASSWORD = '6563291f-aaec-49c4-b63f-45fbbc0e1fe3';
-const vgs_outbound_url = `${VGS_VAULT_ID}.sandbox.verygoodproxy.com`;
-console.log(`Sending request through outbound Route: ${vgs_outbound_url}`);
+const STRIPE_KEY = 'sk_test_51Lrs6CK6opjUgeSmFHReX14eBMcbofCJrUOisGTC7ASpkfFMqD6Eysbs83qBC12YZErV3nv1Pg4UTy9WRhPRVUpQ00o7cUrV8I';
 
-// Create a tunneling agent for HTTPS requests
-const agent = tunnel.httpsOverHttps({
-    proxy: {
-        servername: vgs_outbound_url,
-        host: vgs_outbound_url,
-        port: 8443,
-        proxyAuth: `${VGS_USERNAME}:${VGS_PASSWORD}`
-    },
-});
+console.log(`Outbound route certificate is stored at this path: ${process.env['NODE_EXTRA_CA_CERTS']}`);
 
-app.use(express.json());
+function getProxyAgent() {
+    const vgs_outbound_url = `${VGS_VAULT_ID}.sandbox.verygoodproxy.com`
+    console.log(`Sending request through outbound Route: ${vgs_outbound_url}`);
+    return tunnel.httpsOverHttps({
+        proxy: {
+            servername: vgs_outbound_url,
+            host: vgs_outbound_url,
+            port: 8443,
+            proxyAuth: `${VGS_USERNAME}:${VGS_PASSWORD}`
+        },
+    });
+}
 
-// Route to send data to outbound route
-app.post('/process-payment', async (req, res) => {
+async function postStripePayment(creditCardInfo) {
+    let agent = getProxyAgent();
+    let expiry = creditCardInfo['card-expiration-date'].split('/');
+
+    let buff = Buffer.from(STRIPE_KEY + ":");
+    let base64Auth = buff.toString('base64');
+
+    const instance = axios.create({
+        baseURL: 'https://api.stripe.com',
+        headers: {
+            'Authorization': `Basic ${base64Auth}`,
+        },
+        httpsAgent: agent,
+    });
+
     try {
-        console.log('Incoming request data:', req.body);
+        let pm_response = await instance.post('/v1/payment_methods', qs.stringify({
+            type: 'card',
+            card: {
+                number: creditCardInfo['card-number'],
+                cvc: creditCardInfo['card-security-code'],
+                exp_month: expiry[0].trim(),
+                exp_year: expiry[1].trim()
+            }
+        }));
+        console.log(pm_response.data);
 
-        // Send data to VGS Outbound Route
-        const vgsResponse = await axios.post(`https://${vgs_outbound_url}/post`, req.body, {
-            httpsAgent: agent
-        });
+        let pi_response = await instance.post('/v1/payment_intents', qs.stringify({
+            amount: 100,
+            currency: 'usd',
+            payment_method: pm_response.data.id,
+            confirm: true
+        }));
+        console.log(pi_response.data);
 
-        console.log('VGS Response:', vgsResponse.data);
-
-        res.status(200).send('Data sent to outbound route successfully.');
+        return pi_response.data;
     } catch (error) {
-        console.error('Error sending data to outbound route:', error.message);
-        res.status(500).json({ error: 'An error occurred while sending data to outbound route.' });
+        console.error('Error processing payment:', error.response.data);
+        throw error;
     }
-});
+}
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+module.exports = postStripePayment;
