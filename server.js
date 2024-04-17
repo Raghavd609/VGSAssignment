fedbf888cf1cdee7f12b3bf92cdde0ac96480c44
Raghav Dave <raghavdave@Raghavs-MacBook-Pro.local>
2024-04-17 02:21:02 -0400

    tes

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.post('/process-payment', async (req, res) => {
     console.log('Received request:', req.body);
 
-    // Attempt to parse the data if it's a string, otherwise use it directly if it's already an object
-    const creditCardData = typeof req.body.data === 'string' ? JSON.parse(req.body.data) : req.body.data;
-    
+    // Accessing the data directly from req.body.json assuming your log prints out req.body
+    const creditCardData = req.body.json;
 
     if (!creditCardData || !creditCardData.cc_exp || !creditCardData.cc_number || !creditCardData.cc_cvv) {
         console.error('Invalid or missing credit card information');
@@ -23,15 +22,17 @@ app.post('/process-payment', async (req, res) => {
 
     console.log('Checking Expiration Date:', creditCardData.cc_exp);
     const expiry = creditCardData.cc_exp.split('/');
-    const exp_month = expiry[0];
-    const exp_year = expiry[1];
+    const exp_month = expiry[0]; // Correctly scope exp_month
+    const exp_year = expiry[1]; // Correctly scope exp_year
 
     if (isNaN(exp_month) || exp_month < 1 || exp_month > 12 || isNaN(exp_year) || exp_year.length !== 2) {
         return res.status(400).json({ error: "Expiration date is out of range or incorrectly formatted" });
     }
 
     try {
-        const paymentResponse = await postStripePayment(creditCardData);
+        const tokenizedData = await tokenizeCreditCardData(creditCardData);
+        console.log('Tokenized Data:', tokenizedData);
+        const paymentResponse = await postStripePayment(tokenizedData, exp_month, exp_year);
         res.status(200).json(paymentResponse);
     } catch (error) {
         console.error('Error processing payment:', error);
@@ -39,7 +40,18 @@ app.post('/process-payment', async (req, res) => {
     }
 });
 
-async function postStripePayment(creditCardData) {
+async function tokenizeCreditCardData(creditCardData) {
+    console.log('STEP 1: OUTBOUND - SENDING CREDIT CARD DATA TO VGS:', creditCardData);
+    const vgsResponse = await axios.post('https://tntkmaqsnf9.sandbox.verygoodproxy.com/post', creditCardData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    console.log('STEP 2: OUTBOUND - RECEIVED TOKENIZED DATA FROM VGS:', vgsResponse.data);
+    return vgsResponse.data;
+}
+
+async function postStripePayment(creditCardData, exp_month, exp_year) {
     const instance = axios.create({
         baseURL: 'https://api.stripe.com',
         headers: {
@@ -47,6 +59,7 @@ async function postStripePayment(creditCardData) {
         }
     });
 
+    console.log('STEP 3: OUTBOUND - SENDING TOKENIZED DATA TO STRIPE:', creditCardData);
     const pm_response = await instance.post('/v1/payment_methods', qs.stringify({
         type: 'card',
         card: {
